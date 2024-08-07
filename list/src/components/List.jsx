184199c2b3f93082f8e171bc1999d2ca7a7cf9a1
VIKAS <vikasraw17@gmail.com>
2024-08-07 21:59:27 +0530

    commit

--- list/src/components/List.jsx
+++ list/src/components/List.jsx
@@ -4,8 +4,8 @@ const List = ({ items }) => {
     return (
         <ul style={{ listStyle: 'none', padding: '0' }}>
             {items.map((item, index) => (
-                <li key={index} style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-                    {item.API}
+                <li key={item.fact_id} style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+                    {item.fact}
                 </li>
             ))}
         </ul>
