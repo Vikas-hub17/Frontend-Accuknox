4148fc08a892529016d8bb085c995a306a4e9fd3
VIKAS <vikasraw17@gmail.com>
2024-08-07 11:07:28 +0530

    commit

---
+++ list/src/list.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+function list() {
+  return (
+
+    <div className='container'>
+
+    <div className='header'>
+        <div className='heading'> Accuknox </div>
+    </div>
+
+
+    <div class="wrap">
+   <div class="search">
+      <input type="text" class="searchTerm" placeholder="What are you looking for?" />
+      <button type="submit" class="searchButton">
+        <i class="fa fa-search"></i>
+     </button>
+   </div>
+</div>
+
+    </div>
+
+
+  )
+}
+
+export default list
